04a606ca147754ccfdef6dd61dd67e90e32e4a9c
Jose Lo <joalloce@gmail.com>
2022-10-25 22:59:24 -0600

    made the connection and env variables done.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,16 @@
-const accessEnv = require("./helpers/accessEnv");
+require("dotenv").config();
+
 const redis = require("redis");
 
-const REDIS_URL = accessEnv("REDIS_URL", "url");
-const REDIS_PASSWORD = accessEnv("REDIS_PASSWORD", "password");
+const REDIS_URL = process.env.REDIS_URL;
+const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
 
 console.log(REDIS_URL, REDIS_PASSWORD);
 const client = redis.createClient({
   url: REDIS_URL,
   password: REDIS_PASSWORD,
 });
-console.log("Hello");
+
+(async () => await client.connect())();
+
+client.on("connect", () => console.log("connected"));
